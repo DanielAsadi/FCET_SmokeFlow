2a73cac863b86d83118473633749e21e430ef2b3
dan7-x <55293331+dan7-x@users.noreply.github.com>
2021-07-23 15:07:36 -0400

    Did some tests, not needed currently

--- A_control_manual_test/A_control_manual_test.ino
+++ A_control_manual_test/A_control_manual_test.ino
@@ -26,35 +26,35 @@
 #include <SPI.h>
 
 /* Serial rates for UART */
-#define BAUDRATE        115200
+#define BAUDRATE 115200
 
 /* SPI commands */
-#define AMT22_NOP       0x00
-#define AMT22_RESET     0x60
-#define AMT22_ZERO      0x70
+#define AMT22_NOP 0x00
+#define AMT22_RESET 0x60
+#define AMT22_ZERO 0x70
 
 /* Define special ascii characters */
-#define NEWLINE         0x0A
-#define TAB             0x09
+#define NEWLINE 0x0A
+#define TAB 0x09
 
 /* We will use these define macros so we can write code once compatible with 12 or 14 bit encoders */
-#define RES12           12
-#define RES14           14
+#define RES12 12
+#define RES14 14
 
 /* SPI pins */
-#define ENC_0            32 //YELLOW CABLE
-#define ENC_1            1 //ALT ENCODER, NOT USED RN
-#define SPI_MOSI         51 //ORANGE CABLE
-#define SPI_MISO         50 //GREEN CABLE
-#define SPI_SCLK         52 //BROWN CABLE
-#define LEDY             22 //VALVE STATUS
-#define LEDR             23 //CAM STATUS
-#define LEDG             24 //WIRE STATUS
-#define LEDB             25 //CAP STATUS
-#define VALVE            13 //PWM
-#define CAM              12 //PWM
-#define WIRE             11 //PWM
-#define CAP              10 //PWM
+#define ENC_0 32    //YELLOW CABLE
+#define ENC_1 1     //ALT ENCODER, NOT USED RN
+#define SPI_MOSI 51 //ORANGE CABLE
+#define SPI_MISO 50 //GREEN CABLE
+#define SPI_SCLK 52 //BROWN CABLE
+#define LEDY 22     //VALVE STATUS
+#define LEDR 23     //CAM STATUS
+#define LEDG 24     //WIRE STATUS
+#define LEDB 25     //CAP STATUS
+#define VALVE 13    //PWM
+#define CAM 12      //PWM
+#define WIRE 11     //PWM
+#define CAP 10      //PWM
 
 //create a 16 bit variable to hold the encoders position
 uint16_t encoderPosition;
@@ -93,7 +93,7 @@ void setup()
   //set the clockrate. Uno clock rate is 16Mhz, divider of 32 gives 500 kHz.
   //500 kHz is a good speed for our test environment
   //SPI.setClockDivider(SPI_CLOCK_DIV2);   // 8 MHz
-  SPI.setClockDivider(SPI_CLOCK_DIV4);   // 4 MHz
+  SPI.setClockDivider(SPI_CLOCK_DIV4); // 4 MHz
   //SPI.setClockDivider(SPI_CLOCK_DIV8);   // 2 MHz
   //SPI.setClockDivider(SPI_CLOCK_DIV16);  // 1 MHz
   //SPI.setClockDivider(SPI_CLOCK_DIV32);    // 500 kHz
@@ -109,7 +109,7 @@ void loop()
   //if you want to set the zero position before beggining uncomment the following function call
   //setZeroSPI(ENC_0);
   //setZeroSPI(ENC_1);
-  
+
   attempts = 0; //set attemps counter at 0 so we can try again if we get bad position
 
   //this function gets the encoder position and returns it as a uint16_t
@@ -118,55 +118,77 @@ void loop()
 
   //if the position returned was 0xFFFF we know that there was an error calculating the checksum
   //make 3 attempts for position. we will pre-increment attempts because we'll use the number later and want an accurate count
-  while (encoderPosition == 0xFFFF && ++attempts < 3){
-    encoderPosition = getPositionSPI(ENC_0, RES14);} //try again
-  
-  if (encoderPosition == 0xFFFF){ //position is bad, let the user know how many times we tried
+  while (encoderPosition == 0xFFFF && ++attempts < 3)
+  {
+    encoderPosition = getPositionSPI(ENC_0, RES14);
+  } //try again
+
+  if (encoderPosition == 0xFFFF)
+  { //position is bad, let the user know how many times we tried
     Serial.print("Encoder 0 error. Attempts: ");
     Serial.print(attempts, DEC); //print out the number in decimal format. attempts - 1 is used since we post incremented the loop
-    Serial.write("\r\n");}
-  
-  else { //position was good, print to serial stream
-    if(0< encoderPosition <1000) {
+    Serial.write("\r\n");
+  }
 
+  else
+  { //position was good, print to serial stream
+    Serial.println(encoderPosition, DEC);
+    float a = encoderPosition;
+    if(0 < a < 2000)
+    {
+      digitalWrite(LEDB, HIGH);
     }
-    //Serial.println(encoderPosition, DEC);} //print the position in decimal format -----TESTING----
-  
-  if (Serial.available() > 0) {
+    else
+    {
+      digitalWrite(LEDB, LOW);
+    }
+    //Serial.println(encoderPosition, DEC); //print the position in decimal format -----TESTING-----
+  }
+
+  if (Serial.available() > 0)
+  {
     incomingByte = Serial.read(); // read the oldest byte in the serial buffer:
 
-    if (incomingByte == 'A') {
+    if (incomingByte == 'A')
+    {
       digitalWrite(VALVE, HIGH);
       digitalWrite(LEDY, HIGH);
     }
-    else if (incomingByte == 'B') {
+    else if (incomingByte == 'B')
+    {
       digitalWrite(VALVE, LOW);
       digitalWrite(LEDY, LOW);
     }
 
-    if (incomingByte == 'C') {
+    if (incomingByte == 'C')
+    {
       digitalWrite(CAM, HIGH);
       digitalWrite(LEDR, HIGH);
     }
-    else if (incomingByte == 'D') {
+    else if (incomingByte == 'D')
+    {
       digitalWrite(CAM, LOW);
       digitalWrite(LEDR, LOW);
     }
 
-    if (incomingByte == 'E') {
+    if (incomingByte == 'E')
+    {
       digitalWrite(WIRE, LOW);
       digitalWrite(LEDG, HIGH);
     }
-    else if (incomingByte == 'F') {
+    else if (incomingByte == 'F')
+    {
       digitalWrite(WIRE, HIGH);
       digitalWrite(LEDG, LOW);
     }
 
-    if (incomingByte == 'G') {
+    if (incomingByte == 'G')
+    {
       digitalWrite(CAP, HIGH);
       digitalWrite(LEDB, HIGH);
     }
-    else if (incomingByte == 'H') {
+    else if (incomingByte == 'H')
+    {
       digitalWrite(CAP, LOW);
       digitalWrite(LEDB, LOW);
     }
@@ -187,8 +209,8 @@ void loop()
 */
 uint16_t getPositionSPI(uint8_t encoder, uint8_t resolution)
 {
-  uint16_t currentPosition;       //16-bit response from encoder
-  bool binaryArray[16];           //after receiving the position we will populate this array and use it for calculating the checksum
+  uint16_t currentPosition; //16-bit response from encoder
+  bool binaryArray[16];     //after receiving the position we will populate this array and use it for calculating the checksum
 
   //get first byte which is the high byte, shift it 8 bits. don't release line for the first byte
   currentPosition = spiWriteRead(AMT22_NOP, encoder, false) << 8;
@@ -202,11 +224,11 @@ uint16_t getPositionSPI(uint8_t encoder, uint8_t resolution)
   currentPosition |= spiWriteRead(AMT22_NOP, encoder, true);
 
   //run through the 16 bits of position and put each bit into a slot in the array so we can do the checksum calculation
-  for (int i = 0; i < 16; i++) binaryArray[i] = (0x01) & (currentPosition >> (i));
+  for (int i = 0; i < 16; i++)
+    binaryArray[i] = (0x01) & (currentPosition >> (i));
 
   //using the equation on the datasheet we can calculate the checksums and then make sure they match what the encoder sent
-  if ((binaryArray[15] == !(binaryArray[13] ^ binaryArray[11] ^ binaryArray[9] ^ binaryArray[7] ^ binaryArray[5] ^ binaryArray[3] ^ binaryArray[1]))
-      && (binaryArray[14] == !(binaryArray[12] ^ binaryArray[10] ^ binaryArray[8] ^ binaryArray[6] ^ binaryArray[4] ^ binaryArray[2] ^ binaryArray[0])))
+  if ((binaryArray[15] == !(binaryArray[13] ^ binaryArray[11] ^ binaryArray[9] ^ binaryArray[7] ^ binaryArray[5] ^ binaryArray[3] ^ binaryArray[1])) && (binaryArray[14] == !(binaryArray[12] ^ binaryArray[10] ^ binaryArray[8] ^ binaryArray[6] ^ binaryArray[4] ^ binaryArray[2] ^ binaryArray[0])))
   {
     //we got back a good position, so just mask away the checkbits
     currentPosition &= 0x3FFF;
@@ -217,7 +239,8 @@ uint16_t getPositionSPI(uint8_t encoder, uint8_t resolution)
   }
 
   //If the resolution is 12-bits, and wasn't 0xFFFF, then shift position, otherwise do nothing
-  if ((resolution == RES12) && (currentPosition != 0xFFFF)) currentPosition = currentPosition >> 2;
+  if ((resolution == RES12) && (currentPosition != 0xFFFF))
+    currentPosition = currentPosition >> 2;
 
   return currentPosition;
 }
@@ -235,7 +258,7 @@ uint8_t spiWriteRead(uint8_t sendByte, uint8_t encoder, uint8_t releaseLine)
   uint8_t data;
 
   //set cs low, cs may already be low but there's no issue calling it again except for extra time
-  setCSLine(encoder , LOW);
+  setCSLine(encoder, LOW);
 
   //There is a minimum time requirement after CS goes low before data can be clocked out of the encoder.
   //We will implement that time delay here, however the arduino is not the fastest device so the delay
@@ -244,7 +267,7 @@ uint8_t spiWriteRead(uint8_t sendByte, uint8_t encoder, uint8_t releaseLine)
 
   //send the command
   data = SPI.transfer(sendByte);
-  delayMicroseconds(3); //There is also a minimum time after clocking that CS should remain asserted before we release it
+  delayMicroseconds(3);            //There is also a minimum time after clocking that CS should remain asserted before we release it
   setCSLine(encoder, releaseLine); //if releaseLine is high set it high else it stays low
 
   return data;
@@ -254,7 +277,7 @@ uint8_t spiWriteRead(uint8_t sendByte, uint8_t encoder, uint8_t releaseLine)
    This function sets the state of the SPI line. It isn't necessary but makes the code more readable than having digitalWrite everywhere
    This function takes the pin number of the desired device as an input
 */
-void setCSLine (uint8_t encoder, uint8_t csLine)
+void setCSLine(uint8_t encoder, uint8_t csLine)
 {
   digitalWrite(encoder, csLine);
 }
